0545d785bc9ead44daa57eb23ea15aa598cd1a41
Daniel Fernandez Perez <daniel.fdz.perez@gmail.com>
2014-01-22 10:37:30 +0100

    soluciones

---
+++ solucion/01_simple_ouput/ouput.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+
+   
+    for (int repeticion=0; repeticion<10; repeticion++)
+	printf(" %s\n", argv[1]);
+
+
+    return EXIT_SUCCESS;
+}
